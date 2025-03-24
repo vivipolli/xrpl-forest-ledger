c2cd68ac7fd1735f8f9499dcb831a4f919e9a5a4
vivi <pollivivian@gmail.com>
2025-03-24 13:22:52 -0300

    remove nodemodules

--- frontend/node_modules/@everstake/wallet-sdk-solana/src/__tests__/index.ts
+++
@@ -1,8 +0,0 @@
-import { Solana } from '..';
-
-describe('Solana', () => {
-  it('should create a new Solana instance', () => {
-    const solana = new Solana();
-    expect(solana).toBeInstanceOf(Solana);
-  });
-});
